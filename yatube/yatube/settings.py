bc64b013951917a2a18c8b4b9110cb236270a317
Kirill <yand.kiryushin@ya.ru>
2023-04-14 20:00:03 +0300

    Четвертое изменение

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -40,7 +40,13 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '[::1]',
     'testserver',
+    'www.kirillka06.pythonanywhere.com',
+    'kirillka06.pythonanywhere.com',
 ]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+] 
 
 
 # Application definition
@@ -57,6 +63,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +74,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
